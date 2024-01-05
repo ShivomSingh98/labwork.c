fcb113ba4226050e17872b643f50e2c1bedaf6e9
ShivomSingh98 <148426988+ShivomSingh98@users.noreply.github.com>
2024-01-05 11:29:54 +0530

    Create prime no.c

---
+++ prime no.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+  int i,n=2,a,prime=0;
+printf("Enter the number:");
+scanf("%d",&i);
+while(n<i)
+{
+if(i%n==0)
+{
+prime++;
+}
+n++;
+}
+if(prime==0)
+{
+printf("This number is Prime.\n");
+}
+else
+{
+printf("This number is not Prime.\n");
+}
+}
