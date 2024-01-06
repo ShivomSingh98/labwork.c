c314e0f2435ccdb921f6a793869ac4ba8b4fe7d4
ShivomSingh98 <148426988+ShivomSingh98@users.noreply.github.com>
2024-01-06 09:26:35 +0530

    Create Eligibilty age.c

---
+++ Eligibilty age.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("Enter the age");
+scanf("%d",&a);
+if("a>=18&&a<=50")
+{
+if("a>=18&&a<=20")
+{
+printf("Eligibility od job is in Grade A");
+}
+else
+{
+printf("Eligibility of job is in Grade B");
+}
+}
+else
+{
+printf("Not eligible for the job");
+}
+}
